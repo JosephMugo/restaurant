530b11f76e6342aa6c04c164da9d58a202cfddc4
JosephMugo <joemu18@gmail.com>
2020-11-05 23:00:25 -0600

    added styling for hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './style/Main.scss';
 
 const HeroSection = () => {
     return (
-        <div className="hero-section-container">
-
+        <div className="row justify-content-md-center hero-section-container">
+            <div className="container text-center">
+                <h1>dine</h1>
+                <h5>Exquisite dining since 1989</h5>
+                <p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+                <div className="button text-uppercase">Book A Table</div>
+            </div>
         </div>
     )
 }
+
+export default HeroSection;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './mainStyle/index.scss';
+// Hero Section Import
+import HeroSection from './components/HeroSection/HeroSection';
+
 class Main extends React.Component {
     render() {
         return(
-            <>
-            </>
+            <div className="container-fluid">  
+                <HeroSection />
+            </div>
         )
     }
 }
